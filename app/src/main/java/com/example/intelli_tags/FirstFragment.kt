25ea1a448056469fd33ecc9d520b0737b13678de
Aniket Khajanchi <ani.khajanchi257@gmail.com>
2021-11-22 03:52:27 +0530

    Text topic API working and copy to clipboard option integrated

--- app/src/main/java/com/example/intelli_tags/FirstFragment.kt
+++ app/src/main/java/com/example/intelli_tags/FirstFragment.kt
@@ -1,20 +1,37 @@
 package com.example.intelli_tags
 
+import android.R.attr
+import android.annotation.SuppressLint
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.fragment_first.view.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.withContext
+import org.json.JSONArray
 import org.json.JSONObject
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.StringBuilder
+import java.lang.reflect.Method
+import android.R.attr.label
+
+import android.content.ClipData
+import androidx.core.content.ContextCompat
+
+import androidx.core.content.ContextCompat.getSystemService
+
 
 class FirstFragment : Fragment(R.layout.fragment_first) {
 
@@ -33,7 +50,17 @@ class FirstFragment : Fragment(R.layout.fragment_first) {
             Log.i("text", text)
             processText(text)
         }
+
         return viewOfLayout
+    }
+
+    private fun copy_to_clipboard(topics: String) {
+        val textToCopy = topics
+
+        val clipboard =
+            getSystemService(requireContext(), ClipboardManager::class.java) as ClipboardManager
+        val clip = ClipData.newPlainText("label", textToCopy)
+        clipboard!!.setPrimaryClip(clip)
     }
 
     private fun processText(text: String) {
@@ -59,19 +86,22 @@ class FirstFragment : Fragment(R.layout.fragment_first) {
 
         Log.i("body", body.toString())
 
-
+//
         val queue = Volley.newRequestQueue(viewOfLayout.context)
+        var response = ""
         val req = object : JsonObjectRequest(
             Method.POST, url, body,
             {
                 Log.i("inside", "Inside Api Call")
 
-                val response = it.getString("jobIdentifier")
+                response = it.getString("jobIdentifier")
+                Handler().postDelayed({ getTopics(response) }, 3000)
                 Log.i("identifier", response)
-                Toast.makeText(requireContext(), "Api Call Failed", Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), "Api Call success", Toast.LENGTH_SHORT).show()
 
             }, {
                 Toast.makeText(requireContext(), "Api Call Failed", Toast.LENGTH_SHORT).show()
+                Log.i("status code", it.message.toString())
             }) {
             override fun getHeaders(): MutableMap<String, String> {
                 val headerMap = mutableMapOf<String, String>()
@@ -83,5 +113,44 @@ class FirstFragment : Fragment(R.layout.fragment_first) {
             }
         }
         queue.add(req)
+    }
+
+    private fun getTopics(response: String) {
+        Log.i("status", response.toString())
+
+        val queue2 = Volley.newRequestQueue(viewOfLayout.context)
+        val req = object : JsonObjectRequest(
+            Method.GET, "https://app.modzy.com/api/results/$response", null,
+            {
+                Log.i("inside", "Inside Api Call")
+                val topics = (it.getJSONObject("results")).getJSONObject("my-input")
+                    .getJSONArray("results.json")
+                Log.i("topics", topics.toString())
+                var output = StringBuilder()
+                for (i in 0 until topics.length())
+                    output.append("#").append(topics[i]).append("\n")
+                Log.i("topics", output.toString())
+                viewOfLayout.findViewById<TextView>(R.id.resultTopics).text = output
+                viewOfLayout.copy.setOnClickListener {
+                    copy_to_clipboard(output.toString())
+                }
+//                copy_to_clipboard(output.toString())
+                Toast.makeText(requireContext(), "Api Call success", Toast.LENGTH_SHORT).show()
+
+            }, {
+                Toast.makeText(requireContext(), "Api Call Failed second", Toast.LENGTH_SHORT)
+                    .show()
+                Log.i("status code", it.message.toString())
+            }) {
+            override fun getHeaders(): MutableMap<String, String> {
+                val headerMap = mutableMapOf<String, String>()
+                headerMap["Authorization"] = "ApiKey KSQslWseSzQ3hfcWeC0A.lMIZQC7rTsApVTnDeArW"
+                headerMap["Content-Type"] = "application/json"
+                headerMap["Accept"] = "application/json"
+                headerMap["User-Agent"] = "PostmanRuntime/7.28.4"
+                return headerMap
+            }
+        }
+        queue2.add(req)
     }
 }
