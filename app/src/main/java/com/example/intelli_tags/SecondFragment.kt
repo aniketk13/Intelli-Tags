1e33705e61625867492b3d3e3f1ec0517c892db3
itsnitish22 <nitish.sharma1186@gmail.com>
2021-11-24 21:10:18 +0530

    added body in SecondFragment.kt

--- app/src/main/java/com/example/intelli_tags/SecondFragment.kt
+++ app/src/main/java/com/example/intelli_tags/SecondFragment.kt
@@ -16,6 +16,7 @@ import com.amplifyframework.AmplifyException
 import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
 import com.amplifyframework.core.Amplify
 import com.amplifyframework.storage.s3.AWSS3StoragePlugin
+import org.json.JSONObject
 
 
 /**
@@ -24,6 +25,7 @@ import com.amplifyframework.storage.s3.AWSS3StoragePlugin
 class SecondFragment : Fragment() {
 
     private lateinit var viewOfLayout2nd: View
+    lateinit var exampleFile: File
     private var imageUri: Uri? = null
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -49,7 +51,7 @@ class SecondFragment : Fragment() {
     }
 
     private fun uploadFile() {
-        val exampleFile = File(requireContext().filesDir, "config.json")
+        exampleFile = File(requireContext().filesDir, "config.json")
         exampleFile.writeText("{\"languages\": [\"eng\", \"deu\", \"fra\", \"ita\", \"lat\", \"por\", \"spa\"] }")
 
         Amplify.Storage.uploadFile("config.json", exampleFile,
@@ -78,6 +80,73 @@ class SecondFragment : Fragment() {
         if (stream != null) {
             Amplify.Storage.uploadInputStream("Image.png", stream, {
                 Log.i("MyAmplifyApp", "Successfully uploaded: ${it.key}")
+
+                //api call to get the jobId
+
+                //body
+                val finalBody = JSONObject()
+                val temp1 = JSONObject()
+                temp1.put("identifier", "c60c8dbd79")
+                temp1.put("version", "0.0.2")
+
+                finalBody.put("model", temp1)
+
+                val temp2 = JSONObject()
+                temp2.put("bucket", "modzybucket35738-dev")
+                temp2.put("key", "public/config.json")
+
+                val confiWaliBody = JSONObject()
+
+                //daalni hai
+                confiWaliBody.put("config.json", temp2)
+
+                val temp3 = JSONObject()
+                temp3.put("bucket", "modzybucket35738-dev")
+                temp3.put("key", "public/Image.png")
+
+                val inputWaliBody = JSONObject()
+                inputWaliBody.put("input",temp3)
+                inputWaliBody.put("config.json", temp2)
+
+                val body0001 = JSONObject()
+
+
+//                body0001.put("0001",inputWaliBody)
+
+                val sourcesWaliBaat = JSONObject()
+                sourcesWaliBaat.put("0001", inputWaliBody)
+
+                val properInput = JSONObject()
+
+                val tempo = JSONObject()
+                tempo.put("type", "aws-s3")
+                tempo.put("accessKeyID", "AKIATLNIEWDMNMKGF4EF")
+                tempo.put("secretAccessKey", "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")
+                tempo.put("region", "aws-s3")
+                tempo.put("type", "us-east-2")
+                properInput.put("sources", sourcesWaliBaat)
+
+                finalBody.put("input", properInput)
+
+
+
+
+//                val temp3 = JSONObject()
+//                temp3.put("input", "public/Image.png")
+//                temp3.put("config.json", exampleFile)
+//
+//                val temp2 = JSONObject()
+//                temp2.put("my-input", temp3)
+//
+//                val temp4 = JSONObject()
+//                temp4.put("type", "embedded")
+//                temp4.put("sources", temp2)
+
+                //final body
+//                finalBody.put("input", temp4)
+//                Log.i("finalBody", finalBody.toString())
+
+
             }, {
                 Log.e("MyAmplifyApp", "Upload failed", it)
             })
